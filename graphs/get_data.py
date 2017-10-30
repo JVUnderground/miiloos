d21c9a475ce01e80908ab9ff7e306cbfce1368f5
Joao Carneiro <joao.carneiro@gmail.com>
2017-10-29 22:06:33 -0200

    get_date: acquire ComedFiveMinFeed data

---
+++ graphs/get_data.py
@@ -0,0 +1,15 @@
+from graphs.models import comedFiveMinFeed
+
+import requests as r
+import json
+
+url_comed = 'https://rrtp.comed.com/api?type=5minutefeed'
+
+result = r.get(url_comed)
+prices = json.loads(result.content)
+
+for instance in prices:
+    date = instance['millisUTC']
+    price = instance['price']
+
+    ComedFiveMinFeed.objects.create(date=date, price=price)
