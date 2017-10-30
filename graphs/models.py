f6e54ec7909444f6af40b75d3478e21a48a10d57
Joao Carneiro <joao.carneiro@gmail.com>
2017-10-29 22:09:55 -0200

    Made date a primary key

--- graphs/models.py
+++ graphs/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class ComedFiveMinFeed(models.Model):
-    date = models.BigIntegerField()
+    date = models.BigIntegerField(primary_key=True)
     price = models.FloatField()
